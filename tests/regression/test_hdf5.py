2da06cef0ea4244105d494a9730e61ab3d69424b
Michael Lange <michael.lange@imperial.ac.uk>
2014-04-30 16:04:34 +0100

    HDF5: Adding simple tests for scalar and vector output

---
+++ tests/regression/test_hdf5.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pytest
+import os
+
+from firedrake import *
+from tests.common import *
+
+try:
+    import h5py
+except ImportError:
+    h5py = None
+
+_h5file = os.path.join(os.curdir, "test_hdf5.h5")
+
+
+@pytest.fixture
+def filepath():
+    if os.path.exists(_h5file):
+        os.remove(_h5file)
+    return _h5file
+
+
+@pytest.mark.skipif("h5py is None", reason='h5py not available')
+def test_hdf5_scalar(mesh, filepath):
+    fs = FunctionSpace(mesh, "CG", 1)
+    x = Function(fs, name='xcoord')
+    x.interpolate(Expression("x[0]"))
+
+    h5file = File(filepath)
+    h5file << x
+
+    h5out = h5py.File(filepath, 'r')
+    xval = h5out['fields']['xcoord']
+    assert np.max(np.abs(xval - x.dat.data)) < 1e-6
+
+
+@pytest.mark.skipif("h5py is None", reason='h5py not available')
+def test_hdf5_vector(mesh, filepath):
+    mesh = UnitSquareMesh(2, 2)
+    h5file = File(filepath)
+    h5file << mesh.coordinates
+
+    h5out = h5py.File(filepath, 'r')
+    xval = h5out['fields']['Coordinates'][:, 0]
+    x = mesh.coordinates.dat.data[:, 0]
+    assert np.max(np.abs(xval - x)) < 1e-6
+    yval = h5out['fields']['Coordinates'][:, 1]
+    y = mesh.coordinates.dat.data[:, 1]
+    assert np.max(np.abs(yval - y)) < 1e-6
